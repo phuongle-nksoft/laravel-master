4145628e6caa0d089ae63da281497d6e0ddbb1f0
Phuong <leduyphuong64@yahoo.com.vn>
2020-03-07 23:20:24 +0700

    try to migration

--- src/database/migrations/2020_03_06_150330_create_roles_table.php
+++ src/database/migrations/2020_03_06_150330_create_roles_table.php
@@ -13,7 +13,7 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-        Schema::table('roles', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->boolean('is_active');
--- src/database/migrations/2020_03_07_153040_create__n_users_table.php
+++ src/database/migrations/2020_03_07_153040_create__n_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateNUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('Nusers', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
@@ -36,9 +36,9 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('users', function(Blueprint $table) {
+        Schema::table('Nusers', function (Blueprint $table) {
             $table->dropForeign('role_id');
         });
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('Nusers');
     }
 }
