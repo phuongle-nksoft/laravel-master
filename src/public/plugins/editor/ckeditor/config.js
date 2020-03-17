864989118e6c5b6d6a1bd67ce3cf40f602c3ea97
phuong.duy <phuong.duy@steinsvik.vn>
2020-03-18 00:47:25 +0700

    updated editor

--- src/public/plugins/editor/ckeditor/config.js
+++ src/public/plugins/editor/ckeditor/config.js
@@ -3,26 +3,26 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
+CKEDITOR.editorConfig = function (config) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
 	config.allowedContent = true;
-    config.basicEntities = false;
-    config.entities = false;
-    config.entities_greek = false;
-    config.entities_latin = false;
-    config.htmlEncodeOutput = false;
-    config.entities_processNumerical = false;
+	config.basicEntities = false;
+	config.entities = false;
+	config.entities_greek = false;
+	config.entities_latin = false;
+	config.htmlEncodeOutput = false;
+	config.entities_processNumerical = false;
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
 
-	config.filebrowserBrowseUrl = '/backend/editor/ckfinder/ckfinder.html';
-   	config.filebrowserImageBrowseUrl = '/backend/editor/ckfinder/ckfinder.html?type=Images';
-   	config.filebrowserFlashBrowseUrl = '/backend/editor/ckfinder/ckfinder.html?type=Flash';
-   	config.filebrowserUploadUrl = '/backend/editor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files';
-   	config.filebrowserImageUploadUrl = '/backend/editor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images';
-   	config.filebrowserFlashUploadUrl = '/backend/editor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash';
+	config.filebrowserBrowseUrl = '/nksoft/plugins/editor/ckfinder/ckfinder.html';
+	config.filebrowserImageBrowseUrl = '/nksoft/plugins/editor/ckfinder/ckfinder.html?type=Images';
+	config.filebrowserFlashBrowseUrl = '/nksoft/plugins/editor/ckfinder/ckfinder.html?type=Flash';
+	config.filebrowserUploadUrl = '/nksoft/plugins/editor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files';
+	config.filebrowserImageUploadUrl = '/nksoft/plugins/editor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images';
+	config.filebrowserFlashUploadUrl = '/nksoft/plugins/editor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash';
 };
 
