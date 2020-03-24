0af3f7dacac4c5e31357cd0dc26f651bc44deda9
Phuong <phuong.duy@steinsvik.vn>
2020-03-24 08:27:23 +0700

    updated model

--- src/Models/Users.php
+++ src/Models/Users.php
@@ -2,12 +2,8 @@
 
 namespace Nksoft\Master\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-
-class Users extends Model
+class Users extends NksoftModel
 {
-    use SoftDeletes;
 
     protected $table = 'users';
 
